34becd8dd72c2ffa440d9de168cdb29395979c15
hussamkhatib <hussamkhatib@gmail.com>
2022-09-18 15:23:43 +0530

    init indexedDB

---
+++ src/db.ts
@@ -0,0 +1,24 @@
+import Dexie, { Table } from "dexie";
+
+export interface Product {
+  id?: number;
+  title: string;
+  category: string;
+  subCategory: string;
+}
+
+export class MySubClassedDexie extends Dexie {
+  // 'products' is added by dexie when declaring the stores()
+  // We just tell the typing system this is the case
+  products!: Table<Product>;
+
+  constructor() {
+    super("myDatabase");
+    this.version(1).stores({
+      products: "++id, title,category,subCategory",
+    });
+  }
+}
+
+const db = new MySubClassedDexie();
+export default db;
